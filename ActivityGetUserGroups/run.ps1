e001dd61db0685aa2a5f2be7725d0aca6f92cade
Hunter Williams <hunter@westwilliams.com>
2025-11-04 20:56:50 -0500

    Dynamic user groups

--- ActivityGetUserGroups/run.ps1
+++ ActivityGetUserGroups/run.ps1
@@ -6,11 +6,9 @@ $InformationPreference = 'Continue'
 
 Write-Log "Getting user groups"
 
-$userGroups = @(
-    "UserGroupA"
-    "UserGroupB"
-    "UserGroupC"
-)
+$userGroups = for ($i = 1; $i -le $param.UserGroupCount; $i++) {
+    "UserGroup$i"
+}
 
 Write-Log "Found $($userGroups.Count) user groups"
 return $userGroups
